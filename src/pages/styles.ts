43e64fac6a205761aed5b8f36a691f0d011c27b3
Guilherme Sandi <guilherme_sandi@hotmail.com>
2021-01-21 18:30:03 -0300

    -

--- src/pages/styles.ts
+++ src/pages/styles.ts
@@ -3,21 +3,24 @@ import styled from 'styled-components';
 export const Header = styled.div`
   background: #000000;
   padding: 30px 0;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
 
   header {
     margin: 0 auto;
     padding: 0 53px;
+    height: 82px;
     display: flex;
     align-items: center;
-    justify-content: flex-start;
   }
 `;
 
 export const Form = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: #4f9419;
 
   div {
     margin: 60px 20px;
-    flex: 1;
   }
 `;
